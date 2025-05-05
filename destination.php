a1a289eb5d2c5325fda96e284e3579255f8f5f30
REINE EKOUETH <reineekoueth@gmail.com>
2025-05-05 03:37:13 +0200

    Update destination.php

--- destination.php
+++ destination.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"></head>
-    <link href="" type="text/css">
+    <link href="Style.css" type="text/css">
 <body>
     <header>
         <h3>Choisissez votre séjour et inscrivez-vous dès maintenant !</h3>
